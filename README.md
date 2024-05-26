d8ee92c9b8078b46d9ac9c961c0b9c7aca836c2e
brenorobazza <breno.robazza@hotmail.com>
2024-05-26 14:47:27 -0300

    Criado ambiente virtual e nova funcionalidade de fala

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Assistente Inteligente para domicilios utilizando TVBox
+# Assistente Virtual para domicilios utilizando TVBox
 
-Este projeto tem como objetivo criar um assistente inteligente acessível e open source para domicílios, utilizando para isso uma TVBox.
+Este projeto tem como objetivo criar um assistente virtual inteligente, acessível e open source, para domicílios, utilizando para isso uma TVBox.
+
+## Inicialização
+1) Habilitar ambiente virtual
+
+```bash
+source tvbox/Scripts/activate
+```
+
+2) Iniciar o assistente virtual
+```bash
+python main.py
+```
+
+Na versão atual, o assistente apenas entende o que você disse e repete, tanto em texto quanto em áudio
--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import speech_recognition as sr
+from gtts import gTTS
+from playsound import playsound
+
+
+def cria_audio(audio):
+    tts = gTTS(audio, lang="pt-br")
+    tts.save("audios/phrase.mp3")
+    playsound("audios/phrase.mp3")
 
 def main():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Diga algo")
+        print("Escutando...")
         audio = r.listen(source)
 
     try:
-        print("Você disse: " + r.recognize_google(audio, language='pt-BR'))
+        frase = r.recognize_google(audio, language="pt-BR")
+        print("Você disse: " + frase)
+        cria_audio(frase)
+            
     except sr.UnknownValueError:
         print("Não entendi o que disse")
     except sr.RequestError as e:
         print("Não consegui encontrar resultados; {0}".format(e))
+
 
 if __name__ == "__main__":
     main()
