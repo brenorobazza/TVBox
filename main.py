1c1a1265573ca042134aa16c2295bbe4fc269a4f
brenorobazza <breno.robazza@hotmail.com>
2024-05-30 23:54:49 -0300

    Adicionada verificação para criar pasta 'audios' caso nao exista

--- main.py
+++ main.py
@@ -1,45 +1,78 @@
+import os
 import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
 import skills.clock as clock
 
+AUDIO_PATH = "audios/phrase.mp3"
+
+class COMMAND:
+    """
+    Classe com os comandos padrão que podem ser retornados pelas skills.
+    """
+    DEFAULT = 0
+    DESLIGAR = 1
+
 
 def find_skill(text):
-    if "que horas são" in text.lower():
-        speak = clock.what_time_is_it()
+    """
+    Determina qual skill deve ser chamada com base no texto fornecido.
     
-    #if "..." in text.lower():
-    #    speak = "Resposta da outra skill"
-        
-    else:
-        speak = "Não conheço essa"
-        
-    cria_audio(speak)
-        
-def cria_audio(audio):
-    '''
-    Recebe um texto, cria um arquivo de áudio e o reproduz.
-    '''
-    tts = gTTS(audio, lang="pt-br")
-    tts.save("audios/phrase.mp3")
-    playsound("audios/phrase.mp3")
+    Args:
+        text (str): Texto de entrada.
+    
+    Returns:
+        int: Código de comando.
+    """
+    text = text.lower()
+    text_answer = "Desculpe, não entendi"
+    command = COMMAND.DEFAULT
+    
+    if "que horas são" in text:
+        text_answer = clock.what_time_is_it()
+    elif "desligar" in text:
+        text_answer = "Até logo!"
+        command = COMMAND.DESLIGAR
+    
+    say(text_answer)
+    return command
+
+
+def say(text):
+    """
+    Converte o texto em fala e reproduz o áudio.
+    
+    Args:
+        text (str): Texto a ser convertido em fala.
+    """
+    tts = gTTS(text, lang="pt-br")
+    if not os.path.exists('audios'):
+        os.makedirs('audios')
+    tts.save(AUDIO_PATH)
+    playsound(AUDIO_PATH)
+    
 
 def main():
     r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Escutando...")
-        audio = r.listen(source)
+    running = True
+    
+    while running:
+        with sr.Microphone() as source:
+            print("Escutando...")
+            audio = r.listen(source)
 
-    try:
-        frase = r.recognize_google(audio, language="pt-BR")
-        print("Você disse: " + frase)
-        find_skill(frase)
+        try:
+            frase = r.recognize_google(audio, language="pt-BR")
+            print("Você disse: " + frase)
+            command = find_skill(frase)
             
-    except sr.UnknownValueError:
-        print("Não entendi o que disse")
-    except sr.RequestError as e:
-        print("Não consegui encontrar resultados; {0}".format(e))
-
+            if command == COMMAND.DESLIGAR:
+                running = False
+                
+        except sr.UnknownValueError:
+            print("Não entendi o que disse")
+        except sr.RequestError as e:
+            print(f"Não consegui encontrar resultados; {e}")
 
 if __name__ == "__main__":
     main()
