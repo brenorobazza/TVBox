6ab0e215af35a7eae72ae1c4b4ce73782bb2584e
brenorobazza <breno.robazza@hotmail.com>
2024-05-31 00:00:45 -0300

    Gitignore atualizado

--- main.py
+++ main.py
@@ -5,40 +5,44 @@ from playsound import playsound
 import skills.clock as clock
 import tempfile
 
+
 class COMMAND:
     """
     Classe com os comandos padrão que podem ser retornados pelas skills.
     """
+
     DEFAULT = 0
     DESLIGAR = 1
+
 
 def find_skill(text):
     """
     Determina qual skill deve ser chamada com base no texto fornecido.
-    
+
     Args:
         text (str): Texto de entrada.
-    
+
     Returns:
         int: Código de comando.
     """
     text = text.lower()
     text_answer = "Desculpe, não entendi"
     command = COMMAND.DEFAULT
-    
+
     if "que horas são" in text:
         text_answer = clock.what_time_is_it()
     elif "desligar" in text:
         text_answer = "Até logo!"
         command = COMMAND.DESLIGAR
-    
+
     say(text_answer)
     return command
+
 
 def say(text):
     """
     Converte o texto em fala e reproduz o áudio.
-    
+
     Args:
         text (str): Texto a ser convertido em fala.
     """
@@ -49,10 +53,11 @@ def say(text):
     playsound(temp_path)
     os.remove(temp_path)
 
+
 def main():
     r = sr.Recognizer()
     running = True
-    
+
     while running:
         with sr.Microphone() as source:
             print("Escutando...")
@@ -62,14 +67,15 @@ def main():
             frase = r.recognize_google(audio, language="pt-BR")
             print("Você disse: " + frase)
             command = find_skill(frase)
-            
+
             if command == COMMAND.DESLIGAR:
                 running = False
-                
+
         except sr.UnknownValueError:
             print("Não entendi o que disse")
         except sr.RequestError as e:
             print(f"Não consegui encontrar resultados; {e}")
+
 
 if __name__ == "__main__":
     main()
