fb6f33c3c48734abd0988b1ec81790630c781887
ra198010 <ra198010@students.ic.unicamp.br>
2024-06-02 18:54:25 -0300

    get city

--- skills/temperature.py
+++ skills/temperature.py
@@ -13,14 +13,21 @@ def extract_city_from_text(text):
         return match.group(1).strip()
     return None
 
-def get_temperature(text):
-    
+def get_city(text):
     city = extract_city_from_text(text)
     if city == None:
         city = get_city_from_ip()
+    return city
 
+
+def get_temperature(text):   
+    city = get_city(text)
+    
     url = f"http://wttr.in/{city}?format=%t"
     response = requests.get(url)
     temperature = response.text.strip()
     
     return f"Está {temperature} em {city}" 
+
+def get_weather_forecast(text):
+
