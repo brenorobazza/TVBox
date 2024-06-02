637bba1ed8f70b500f377d90d0836bc09707c087
MilieneRodrigues <m185683@dac.unicamp.br>
2024-06-02 12:29:09 -0300

    Adiciona mais piadas e escolhe piada aleatoria

--- skills/piadocas.py
+++ skills/piadocas.py
@@ -1,3 +1,24 @@
+import random
 
 def get_piada(text):
-    return f"Por que o pássaro não usava chapéu?  \n  Porque ele já tinha penas o suficiente para se proteger do sol!"
+    piadas = piadas = [
+    "Por que o programador foi à praia? \n Porque ele queria navegar na internet!",
+    "Qual é o cúmulo da velocidade? \n Dar a volta na internet!",
+    "O que o Java disse para o C? \n Você não tem classe!",
+    "Por que o computador não sai de casa? \n Porque ele tem Windows!",
+    "O que um loop infinito falou para o outro? \n Vamos dar uma volta e voltar aqui!",
+    "Por que o programador não consegue abrir a porta? \n Porque ele usa o Windows!",
+    "Por que a galinha atravessou o campo de bits? \n Para chegar do outro byte!",
+    "Por que o JavaScript foi à terapia? \n Porque ele tinha problemas com callbacks!",
+    "Por que os desenvolvedores gostam de café gelado? \n Porque eles não curtem Java quente!",
+    "Qual é o animal que mais gosta de Java? \n O Javali!"
+    "Por que o livro de matemática ficou de castigo? \n Porque ele tinha problemas demais!"
+    "O que a mãe celular disse para seu filho quando ele fez uma ligação ruim? \n Você precisa recarregar suas energias, filho!"
+    "Por que o gato não gosta de redes sociais? \n Porque ele prefere o ronronar em vez do retuitar!"
+    "Por que o pássaro estava deprimido? \n Porque ele estava em baixa altitude!"
+    "O que a vaca disse para o terapeuta? \n Eu me sinto como se estivesse sendo ordenhada emocionalmente!"
+    "Por que os pássaros não usam WhatsApp? \n Porque eles já têm Twitter!"
+    ]
+    piada_aleatoria = random.choice(piadas)
+    return piada_aleatoria 
+
